889fd6c830cf60abfc3efae63943a9e16cca3c01
Stefan <stefan.vander.weide@hotmail.com>
2021-03-02 23:14:21 +0100

    Added front-end directory

---
+++ client/src/components/Buttons.vue
@@ -0,0 +1,50 @@
+<template>
+  <transition name="fade" mode="out-in">
+    <div :key="objectURL">
+      <b-button
+        @click="$emit('object-requested')"
+        class="button-standard-primary mr-3"
+        >{{ $t("new-object") }}</b-button
+      >
+      <b-button
+        :href="objectURL"
+        target="_blank"
+        class="button-standard-secondary"
+        >{{ $t("more-information") }}</b-button
+      >
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "Buttons",
+  props: {
+    objectURL: String
+  }
+};
+</script>
+
+<style>
+.button-standard-primary {
+  color: #000;
+  background: #fff;
+  border: solid 2px #ffffff;
+}
+
+.button-standard-primary:hover {
+  background: transparent;
+  border: solid 2px #ffffff;
+}
+
+.button-standard-secondary {
+  background: transparent;
+  border: solid 2px #ffffff;
+}
+
+.button-standard-secondary:hover {
+  color: #000;
+  background: #fff;
+  border: solid 2px #ffffff;
+}
+</style>
